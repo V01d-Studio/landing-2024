02e506cc2512f037f9bdb41c7fbcf83024803760
Samnan Rahee <samnan.rahee.96@gmail.com>
2023-11-11 17:51:56 +0600

    Update(UI): marquee animation for CTA

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,20 @@ export default {
         sans: ["Geist", ...defaultTheme.fontFamily.sans],
         mono: ["Geist Mono", ...defaultTheme.fontFamily.mono],
       },
+      animation: {
+        marquee: "marquee 30s linear infinite",
+        marquee2: "marquee2 30s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
     },
   },
   plugins: [
